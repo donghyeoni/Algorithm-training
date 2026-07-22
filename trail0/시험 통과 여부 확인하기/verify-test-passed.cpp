7e03b4c3fa7b357c5b98c80f4c42f95deb198dab
donghyeoni <korea82hyun@gmail.com>
2026-07-22 21:55:20 +0900

    [Passed] 시험 통과 여부 확인하기 (2ms, 0MB)

---
+++ trail0/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a; 
+    cin >> a; 
+
+    if(a >= 80){
+        cout << "pass" << '\n';
+    }
+    else cout << 80-a << " more score";
+    return 0;
+}
