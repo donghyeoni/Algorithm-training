d44a1ef45cf7b11d8df04e4da1315af1b8c8ff73
donghyeoni <korea82hyun@gmail.com>
2026-07-23 21:25:20 +0900

    [Passed] 문자열 출력하기 (2ms, 0MB)

---
+++ trail0/문자열 출력하기/print-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i ; 
+    cin >> i ; 
+
+    for(int a = 0 ; a < i ; a++){
+        cout << "LeebrosCode" << endl;
+    }
+    return 0;
+}
