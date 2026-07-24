ec223b497211cb72be9768f2b43bb5d00f760aff
donghyeoni <korea82hyun@gmail.com>
2026-07-24 15:19:19 +0900

    [Passed] 더 긴 문자열 (1ms, 0MB)

---
+++ trail0/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string A; 
+    string B; 
+
+    cin >> A >> B; 
+
+    if(A.length() > B.length()) cout << A << ' ' << A.length();
+    else if(A.length() == B.length()) cout << "same";
+    else cout << B << ' ' << B.length();
+
+return 0;
+}
