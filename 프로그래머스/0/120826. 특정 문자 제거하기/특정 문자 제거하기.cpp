8b11aff2ab32e488b1e6244b4a83c55cef5f1390
donghyeoni <korea82hyun@gmail.com>
2026-07-27 22:11:40 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -1,12 +1,13 @@
 #include <string>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string solution(string my_string, string letter) {
-    string answer = "";
-    for(int i = 0 ; i < size(my_string) ; i++){
-        if(my_string[i] != letter[0]) answer.push_back(my_string[i]);
-    }
-    return answer;
+    char target = letter[0];
+    
+    // my_string에서 target과 일치하는 문자를 지우고 크기를 재조정합니다.
+    my_string.erase(remove(my_string.begin(), my_string.end(), target), my_string.end());
+    
+    return my_string;
 }
