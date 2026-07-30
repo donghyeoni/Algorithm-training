034b1180031bdc656cb0cb22640a217ab55dc675
DH KIM <korea82hyun@gmail.com>
2026-07-30 22:56:47 +0900

    [Passed] 특정 원소들의 합 (2ms, 0MB)

---
+++ codetree/trail0/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[4][4];
+
+    for(int i = 0 ; i < 4; i++){
+        for(int j = 0 ; j < 4; j ++){
+            cin >> arr[i][j];
+        }
+    }
+
+    int count = 0 ; 
+
+    for(int i = 0 ; i < 4 ; i++){
+        for( int j = 0 ; j <= i ; j++){
+            count += arr[i][j];
+        }
+    }
+
+    cout << count; 
+    return 0;
+}
