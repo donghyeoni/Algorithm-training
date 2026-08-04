0bfd8a0538709806b61dd97706a45392331fd297
DH KIM <korea82hyun@gmail.com>
2026-08-04 12:15:45 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 5.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    sort(numbers.begin(),numbers.end());
+    answer = numbers[size(numbers) - 1] * numbers[size(numbers) - 2];
+    return answer;
+}
